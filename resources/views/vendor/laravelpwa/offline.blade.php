dea742504978dd4799a115f4736eda2b5b5c187a
rnlyone <ruzmaneman@gmail.com>
2022-02-06 23:11:09 +0800

    Update offline.blade.php

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,27 +1,59 @@
-@include('Layout.app')
 
-<div class="page-content mb-0 pb-0">
-    <div class="card card-style mb-0 bg-transparent shadow-0 bg-20 mx-0 rounded-0" data-card-height="cover">
-        <div class="card-center">
-            <div class="card card-style">
-                <div class="content text-center">
-                    <h1 class="font-28">Kimi wa Offline :(</h1>
-                    <p class="opacity-50">
-                        Sorry, Mungkin Bisa Di Check Internet ta :).
-                    </p>
-                    <div class="text-center pt-3">
-                        <a href="https://www.instagram.com/smuneljc/" target="_blank" class="icon icon-xxs bg-instagram me-2 shadow-bg shadow-bg-xs rounded-s"><i
-                                class="bi bi-instagram color-white font-17"></i></a>
-                        <a href="https://web.facebook.com/nihon05sjc/?_rdc=1&_rdr" target="_blank" class="icon icon-xxs bg-facebook me-2 shadow-bg shadow-bg-xs rounded-s"><i
-                                class="bi bi-facebook color-white font-17"></i></a>
-                    </div>
-                    <h6 class="font-700 text-uppercase pt-2 font-11">Follow Mki Dlu :)</h6>
-                </div>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Offline</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
+    <meta name="theme-color" content="#3f51b5">
+    <link rel="manifest" href="/manifest.json">
+    <style>
+    *{margin:0;padding:0;}
+    body{font-family:arial;box-sizing:border-box;overflow:hidden;}
+    #loader{width:100vw;height:100%;display:flex;justify-content: center;align-items:center;position:absolute;}
+    #spinner{width:40px;height:40px;border-left:7px solid #004f8f; #004f8f;border-right:7px solid transparent;border-bottom:7px solid #004f8f;border-top:7px solid #004f8f;border-radius:50%;animation:rotate 1s infinite forwards;}
+    @keyframes rotate{
+      from{}
+      to{transform:rotate(360deg);}
+    }
+    #wrapper{width:100vw;height:100%;position:fixed;top:0;left:0;right:0;bottom:0;animation:err 0.5s;
+    background:rgba(0,0,0,0.85);}
+    @keyframes err{
+      from{background:#fff}
+      to{background:rgba(0,0,0,0.85);}
+    }
+      #err-div{display:flex;justify-content:space-between;align-items:center; background:#fff;padding:8px;box-sizing:border-box;border-radius:10px 10px 0px 0px;position:fixed;bottom:0;left:0;right:0;z-index:99;animation:popup 0.5s;overflow:hidden}
+      @keyframes popup {
+        from{bottom:-100%}
+        to{bottom:0}
+      }
+
+      #err-text h1{font-size:20px;user-select:none;animation:scale 0.95s}
+      #err-text p{font-size:13px;user-select:none;animation:scale 0.9s}
+      #ok{background:dodgerblue;color:#fff;border:none;outline:none;padding:8px 23px;border-radius:15px;box-shadow:1px 1px 2px 1px rgba(0,0,0,0.2);margin:10px 0px;font-weight:600;animation:scale 1s}
+      #ok:hover,#ok:focus{outline:none;border:none;transform:scale(0.95);box-shadow:1px 1px 2px 1px rgba(0,0,0,0.0)}
+      #img{animation:scale 1s;}
+      @keyframes scale {
+        from{transform:scale(0.7)}
+        to{transform:scale(1)}
+      }
+      #img img{width:190px;user-select:none;pointer-events:none;transform:translate(30px,-12px)scale(1.4)}
+    </style>
+  </head>
+<body>
+  <div id="loader"><div id="spinner"></div></div>
+  <div id="wrapper">
+    <div id="err-div">
+    <div id="err-text">
+    <h1>Layanan sedang diperbaiki.</h1>
+    <p>Jangan panik, mohon tunggu sebentar dan kamu akan segera menikmati layanan kembali</p>
+    <button type="button" onClick="window.location.reload();"; id="ok">OK</button>
     </div>
-</div>
-
-</div>
-
-@include('Layout.footer')
+    <div id="img">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYf3sSvk-re7a8IyKmZMZrSGZHn9JXZehwyg&usqp=CAU"/>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
