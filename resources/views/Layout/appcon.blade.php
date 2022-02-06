a407e296db9beb7ce0dee15377d52514905de625
rnlyone <ruzmaneman@gmail.com>
2022-02-06 21:55:17 +0800

    offliner

--- resources/views/Layout/appcon.blade.php
+++ resources/views/Layout/appcon.blade.php
@@ -18,9 +18,12 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800&amp;family=Roboto:wght@400;500;700&amp;display=swap"
         rel="stylesheet">
-    <link rel="manifest" href="_manifest.json">
+    {{-- <link rel="manifest" href="/manifest.json"> --}}
     <meta id="theme-check" name="theme-color" content="#FFFFFF">
     <link rel="apple-touch-icon" sizes="180x180" href="v30/app/icons/icon-192x192.html">
+    @laravelPWA
+
+
 </head>
 
 <body class="theme-light">
---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,27 @@
+@include('Layout.app')
+
+<div class="page-content mb-0 pb-0">
+    <div class="card card-style mb-0 bg-transparent shadow-0 bg-20 mx-0 rounded-0" data-card-height="cover">
+        <div class="card-center">
+            <div class="card card-style">
+                <div class="content text-center">
+                    <h1 class="font-28">Kimi wa Offline :(</h1>
+                    <p class="opacity-50">
+                        Sorry, Mungkin Bisa Di Check Internet ta :).
+                    </p>
+                    <div class="text-center pt-3">
+                        <a href="https://www.instagram.com/smuneljc/" target="_blank" class="icon icon-xxs bg-instagram me-2 shadow-bg shadow-bg-xs rounded-s"><i
+                                class="bi bi-instagram color-white font-17"></i></a>
+                        <a href="https://web.facebook.com/nihon05sjc/?_rdc=1&_rdr" target="_blank" class="icon icon-xxs bg-facebook me-2 shadow-bg shadow-bg-xs rounded-s"><i
+                                class="bi bi-facebook color-white font-17"></i></a>
+                    </div>
+                    <h6 class="font-700 text-uppercase pt-2 font-11">Follow Mki Dlu :)</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+@include('Layout.footer')
