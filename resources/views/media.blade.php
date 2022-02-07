eb44ce783eb2f5e67ded136e3d2632d01bbc8ad5
rnlyone <ruzmaneman@gmail.com>
2022-02-07 16:30:25 +0800

    media

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -16,7 +16,7 @@
     <div class="card card-style">
         <div class="content">
             <div class="responsive-iframe rounded-s">
-                <iframe class="rounded-s" src="https://upos-bstar1-mirrorakam.akamaized.net/iupxcodeboss/al/qi/n220130ad34c0flkmpv1ig2b7zmmqial-1-112111110023.mp4?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1644220411&gen=playurlv2&os=akam&oi=2362818911&trid=8dcc8915b36a4975acd9a93547f61227i&platform=html5&upsig=8f84946c67a543222253535eb84b5e70&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&hdnts=exp=1644220411~hmac=29918af350c6d4eb8636a66006306eecb9e9e6aa0579439c4f8a5dd582c84d9d&mid=0&bvc=vod&nettype=0&orderid=0,2&logo=00000000" frameborder="0"
+                <iframe class="rounded-s" src="https://mega.nz/embed/yXpmSS7K#iUiqIEqhCd9cHIN9ec7iF7U1Rn_awhTIF_4QgA7BFDM" frameborder="0"
                     allowfullscreen=""></iframe>
             </div>
             <h3 class="mt-3">Miyazono Kaori - Seiyuu Scene</h3>
@@ -28,7 +28,7 @@
     <div class="card card-style">
         <div class="content">
             <div class="responsive-iframe rounded-s">
-                <iframe class="rounded-s" src="http://upos-bstar1-mirrorakam.akamaized.net/iupxcodeboss/wt/zi/n220130addqc9d5zc35dx298zodaziwt-1-112111110023.mp4?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1644221783&gen=playurlv2&os=akam&oi=2362818911&trid=8cd71fd4f4a240469e4ae796256f6a7di&platform=html5&upsig=e3f683dc976ea33974837f2a800ddb42&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&hdnts=exp=1644221783~hmac=9690b6000ca15ddc830f1ba8de747808d54559398676c42f0385b92c97827cbf&mid=0&bvc=vod&nettype=0&orderid=0,2&logo=00000000" frameborder="0"
+                <iframe class="rounded-s" src="https://mega.nz/embed/LXgUFIyB#GrWqeUmrtjAqfEGH2vqahhhuS5s96TWdqMjYdi79BkY" frameborder="0"
                     allowfullscreen=""></iframe>
             </div>
             <h3 class="mt-3">Norman - Seiyuu Scene</h3>
@@ -40,7 +40,7 @@
     <div class="card card-style">
         <div class="content">
             <div class="responsive-iframe rounded-s">
-                <iframe class="rounded-s" src="http://upos-bstar1-mirrorakam.akamaized.net/iupxcodeboss/e0/em/n220130ad303xb1d2561cx27aae1eme0-1-112111110023.mp4?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1644221995&gen=playurlv2&os=akam&oi=2362818911&trid=fcd43dce47104ccf86002bd5cf7d22f6i&platform=html5&upsig=77a301a9721696c1ea2fb61ee5f96d40&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&hdnts=exp=1644221995~hmac=fc78aab820218a05df81796763c4830187a0d27b80b9e6b25ced46e4ac8b826b&mid=0&bvc=vod&nettype=0&orderid=0,2&logo=00000000" frameborder="0"
+                <iframe class="rounded-s" src="https://mega.nz/embed/3ToAnIbK#LS0DSOcrL2TAao8PiA8V5KvURP5byrqAkPF1q0r8z9Y" frameborder="0"
                     allowfullscreen=""></iframe>
             </div>
             <h3 class="mt-3">Shinobu Kocho - Seiyuu Scene</h3>
@@ -52,7 +52,7 @@
     <div class="card card-style">
         <div class="content">
             <div class="responsive-iframe rounded-s">
-                <iframe class="rounded-s" src="http://upos-bstar1-mirrorakam.akamaized.net/iupxcodeboss/2p/wj/n220130ad2iuekruvsca8gu4c86cwj2p-1-112111110023.mp4?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1644222046&gen=playurlv2&os=akam&oi=2362818911&trid=d100d45345af4eada7e0bc74ebb1ad3di&platform=html5&upsig=f0c9806e068472138aa20017c32547b9&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&hdnts=exp=1644222046~hmac=97ffcffbc0e25156212bc8014b2e70864206eed74f870a57bd99f0d41728e550&mid=0&bvc=vod&nettype=0&orderid=0,2&logo=00000000" frameborder="0"
+                <iframe class="rounded-s" src="https://mega.nz/embed/qThE1KDD#7on6TzJcRWaU3QI_F39RVTsN7dLa3avwwShL1dJ31kk" frameborder="0"
                     allowfullscreen=""></iframe>
             </div>
             <h3 class="mt-3">Fujisaki Yuusuke - Seiyuu Scene</h3>
